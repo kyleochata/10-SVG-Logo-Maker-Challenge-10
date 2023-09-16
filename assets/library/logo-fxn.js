cfece57e5928f2b5ac0550e9c1ff0806e16b61f3
kyleochata <kyleochata@gmail.com>
2023-09-15 17:56:35 -0700

    initial commit

---
+++ assets/library/logo-fxn.js
@@ -0,0 +1,17 @@
+/*
+in this logo fxn.js:
+
+will need to have all the functions needed to generate a svg logo
+*/
+//function to render a circle
+
+//function to render a triangle
+
+//function to render a square
+
+
+//function that will take inquirer data and return a <svg> with user input variables from inquirer to then be sent into a writeFile function on the index
+const createLogo = (data) => {
+  return `<svg version='1.1'
+  width='${data}>`
+}
---
+++ index.js
@@ -0,0 +1,61 @@
+/*
+Inquirer will be on index.
+
+create a different js script with combining all the inquirer outputs to send into a svg.logo maker fxn
+
+questions: 
+  1. Ask for text
+  2. Ask for text color: keyword or hex
+  3. Ask for shape: list: circle, triangle, square
+  4. Ask for shape color: keyword or hex
+
+need fs to writetofile named 'logo.svg'
+*/
+const inquirer = require('inquirer');
+const fs = require('fs');
+const logoFxn = require('./assets/utilities/logo-fxn');
+
+const questions = [
+  {
+    type: `input`,
+    message: `Please choose no more than 3 characters to put in your logo.`,
+    name: `text`
+},
+{
+  type:`input`,
+  message:`Please choose what color your text will be. Name of the color or hexadecimal numbers in the format of #{xxxxxx} are acceptable.`,
+  name: `textColor`,
+  default: `green`,
+},
+{
+  type: `list`,
+  message: `Please select a shape.`,
+  choices: [`circle`, new inquirer.Separator(),
+  `triangle`, new inquirer.Separator(),
+  `square`, new inquirer.Separator(),
+  ],
+  name: `shape`,
+  default: `circle`,
+},
+{
+  type: `input`,
+  message: `What color would you like your shape to be? Name of the color or hexadecimal numbers in the format of #{xxxxxx} are acceptable.`,
+  name: `shapeColor`,
+  default: `black`,
+},
+];
+
+//create a function to write a logo.svg file
+
+//create a function initialize the app
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((answers) => {
+      
+    })
+    .catch(err => {
+      throw new Error(err);
+    })
+}
+init();
